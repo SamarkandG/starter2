e99bb63743846f74040a73882c2b3883ee908cf7
SamarkandG <samarkand.guinot@gmail.com>
2022-08-08 02:51:32 +0200

    Calculate pourcentage population function declaration vs expression

--- assignments.js
+++ assignments.js
@@ -1,13 +1,41 @@
-"use strict";
+// "use strict";
 
-function describeCountry(country, population, capitalCity) {
-  const description = `${country} has ${population} million people and its capital city is ${capitalCity}`;
-  return description;
-}
+// function describeCountry(country, population, capitalCity) {
+//   const description = `${country} has ${population} million people and its capital city is ${capitalCity}`;
+//   return description;
+// }
 
-const finland = describeCountry("Finland", 6, "Helsinki");
-console.log(finland);
-const france = describeCountry("France", 65, "Paris");
-console.log(france);
-const sweden = describeCountry("Sweden", 10, "Stockholm");
-console.log(sweden);
+// const finland = describeCountry("Finland", 6, "Helsinki");
+// console.log(finland);
+// const france = describeCountry("France", 65, "Paris");
+// console.log(france);
+// const sweden = describeCountry("Sweden", 10, "Stockholm");
+// console.log(sweden);
+
+// Function DECLARATION vs EXPRESSIONS
+
+// FUNCTION DECLARATION
+
+// function percentageOfWorld(population) {
+//   const percentage = (population / 7900) * 100;
+//   return percentage;
+// }
+
+// const chinaPercentage = percentageOfWorld(1441);
+// console.log(chinaPercentage.toFixed(2) + "%");
+// const peruPercentage = percentageOfWorld(33);
+// console.log(peruPercentage.toFixed(2) + "%");
+// const iranPercentage = percentageOfWorld(83);
+// console.log(iranPercentage.toFixed(2) + "%");
+
+// // FUNCTION EXPRESSION
+
+// const percentageOfWorld2 = function (population) {
+//   return (population / 7900) * 100;
+// };
+// const chinaPercentage2 = percentageOfWorld2(1441);
+// console.log(chinaPercentage2.toFixed(2) + "%");
+// const peruPercentage2 = percentageOfWorld2(33);
+// console.log(peruPercentage2.toFixed(2) + "%");
+// const iranPercentage2 = percentageOfWorld2(83);
+// console.log(iranPercentage2.toFixed(2) + "%");
--- script.js
+++ script.js
@@ -32,3 +32,30 @@
 
 // const samTotalBill = calcTip(30, 0.15);
 // console.log(`The total bill of Sam is ${samTotalBill}$🤑`);
+
+// Function declaration
+
+// Ici on créer la logique de ce qu'on souhaite calculer ou récuperer
+// On veux calculer l'âge de quelqu'un en l'an 2037
+// On créer une fonction qui calcule l'âge avec comme paramètre l'année de naissance de la personne
+// Cette fonction nous permettra d'afficher et recuperer ce que l'année de naissance de la personne MOINS l'année actuelle donne = l'âge de la personne !
+// Pour  "stocker" le résultat de cette opération on créer une variable en dessous qui s'appelle AGE (oui parce qu'on veux calculer l'âge d'une personne grâce à notre fonction de calcul d'âge juste au dessus !)
+
+// function calcAge1(birthyear) {
+//   return 2037 - birthyear;
+// }
+
+// const age1 = calcAge1(1995);
+// console.log(age1);
+
+// // Function expression
+// // Expression produce values
+
+// // Ici on créer la logique de ce qu'on souhaite calculer ou récuperer
+// // On veux calculer l'âge de quelqu'un en l'an 2037
+// const calcAge2 = function (birthyear) {
+//   return 2037 - birthyear;
+// };
+
+// const age2 = calcAge2(1995);
+// console.log(age1, age2);
